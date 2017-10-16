eccb17e1475bede03b4c7505e0c7e8826a09becf
Chase Wedekind <chasewedekind@gmail.com>
2017-10-16 19:36:51 -0400

    5.1 Routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,17 +40,31 @@ class ProductsController < ApplicationController
 
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @product.update(product_params)
+  #       format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @product }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @product.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
+        format.html { redirect_to products_url, notice: 'Product was successfully updated.' }
+        format.json { render :show, status: :ok, location: products_url}
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
+
+
 
   # DELETE /products/1
   # DELETE /products/1.json
