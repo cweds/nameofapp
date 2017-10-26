c01ff4921ff63cf7e504ce9f12e277c053594a93
Chase Wedekind <chasewedekind@gmail.com>
2017-10-26 18:40:32 -0400

    6.15(1)

--- app/models/product.rb
+++ app/models/product.rb
@@ -25,11 +25,11 @@ class Product < ApplicationRecord
 	end
 
 	def views
-    $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
+    # $redis.get("product:#{id}") # this is equivalent to 'GET product:1'
   end
 
   def viewed!
-    $redis.incrby("product:#{id}", 1) # this is equivalent to 'INC product:1'
+    # $redis.incrby("product:#{id}", 1) # this is equivalent to 'INC product:1'
   end
 
 end
